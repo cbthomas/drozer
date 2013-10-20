2e9c84669f4b5bb9beecdf82f32eea20b2180ad9
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-10-20 15:36:45 -0400

    sniffer prints out all packages successfully

--- sniffer/IntentSniffer.py
+++ sniffer/IntentSniffer.py
@@ -1,5 +1,4 @@
 from drozer.modules import Module,common
-from drozer.modules.common import PackageManager
 
 class createSniffer(Module, common.PackageManager):
 	name=""
@@ -11,7 +10,5 @@ class createSniffer(Module, common.PackageManager):
 	path=[]
 	def execute(self, arguments):
 		self.stdout.write("Hello from Drozer\n")
-		installedPackageManager = PackageManager.packageManager(self)
-		installedPackages = installedPackageManager.getPackages(0)
-		for i in installedPackages:
-			self.stdout.write(i)
+		for package in self.packageManager().getPackages():
+			self.stdout.write("Package: %s\n" % package.applicationInfo.packageName)
