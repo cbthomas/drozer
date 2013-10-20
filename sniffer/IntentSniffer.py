c2673e1b20f1579aa33c53be5d35d00464347252
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-10-20 16:54:06 -0400

    fully parse all manifests for intent-filters

--- sniffer/IntentSniffer.py
+++ sniffer/IntentSniffer.py
@@ -1,6 +1,6 @@
 from drozer.modules import Module,common
 
-class createSniffer(Module, common.PackageManager):
+class createSniffer(Module, common.PackageManager, common.Assets):
 	name=""
 	description=""
 	examples=""
@@ -8,7 +8,26 @@ class createSniffer(Module, common.PackageManager):
 	date=""
 	license=""
 	path=[]
+	
 	def execute(self, arguments):
-		self.stdout.write("Hello from Drozer\n")
+		global outputfile 
+		outputfile = open("/home/parallels/Desktop/intentFilters.txt","w")
+		self.stdout.write("Hello from Drozer\n")	
 		for package in self.packageManager().getPackages():
+			packageNameString = package.applicationInfo.packageName
 			self.stdout.write("Package: %s\n" % package.applicationInfo.packageName)
+			self.parse(self.getAndroidManifest(packageNameString))
+		outputfile.close()
+	def parse(self, manifest):
+		manifestSplit = manifest.split("\n")
+		startWriting = 0
+		for line in manifestSplit:
+			if(line == "<intent-filter>"):
+				startWriting = 1
+				outputfile.write(line + "\n")
+			elif(line == "</intent-filter>"):
+				startWriting = 0
+				outputfile.write(line + "\n")
+			elif(startWriting == 1):
+				outputfile.write(line + "\n")
+			
